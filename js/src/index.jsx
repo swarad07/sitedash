3c24f5606ca96025e67c3581c1d5c3fad1588af7
Swarad Mokal <swarad07@gmail.com>
2021-12-01 12:39:11 +0530

    initial commit

---
+++ js/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Dashboard from './components/dashboard';
+
+ReactDOM.render(
+  <Dashboard />,
+  document.getElementById('site-dashboard')
+);
